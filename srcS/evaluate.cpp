7b27a904fd7131887d4245e37a9e5fc362c940cb
Marco Zerbinati <zerbinati.marco@gmail.com>
2016-05-08 14:08:39 +0200

    Unsafe checks
    Unsafe checks
    Introducing a new multi-purpose penalty related to King safety, which
    includes all kind of potential checks (from unsafe or unavailable
    squares currently occupied by some other piece)
    
    This will indirectly detect and reward some pins, discovered checks, and
    motifs such as square vacation, or rook behind its pawn and aligned with
    King (example Black Rg8, g7 against Kg1),
    and penalize some pawn blockers (if they move, it allows a discovered
    check by the pawn).
    
    And since it looks also at protected squares, it detects some potential
    defense overloading.
    
    Finally, the rook contact checks had been removed some time ago. This
    test will give a small bonus for them, as well as for bishop contact
    checks.
    
    Passed STC
    http://tests.stockfishchess.org/tests/view/5729ec740ebc59301a354b36
    LLR: 2.94 (-2.94,2.94) [0.00,5.00]
    Total: 13306 W: 2477 L: 2296 D: 8533
    
    and LTC
    http://tests.stockfishchess.org/tests/view/572a5be00ebc59301a354b65
    LLR: 2.97 (-2.94,2.94) [0.00,5.00]
    Total: 20369 W: 2750 L: 2565 D: 15054
    
    bench: 9298175
    master (#28)
    1 parent 5e4cd3f commit 4b9ed6566a7ed026160fc79b778f95e62fde22a1
    @Rocky640 Rocky640 committed

--- srcS/evaluate.cpp
+++ srcS/evaluate.cpp
@@ -185,11 +185,12 @@ namespace {
   const Score BishopPawns        = S( 8, 12);
   const Score MinorBehindPawn    = S(16,  0);
   const Score TrappedRook        = S(92,  0);
+  const Score SafeCheck          = S(20, 20);
+  const Score OtherCheck         = S(10, 10);
   const Score LooseEnemies       = S( 0, 25);
   const Score Unstoppable        = S( 0, 20);
         Score Hanging            = S(48, 27);
   const Score PawnAttackThreat   = S(38, 22);
-  const Score Checked            = S(20, 20);
 
   // Penalty for a bishop on a1/h1 (a8/h8 for black) which is trapped by
   // a friendly pawn on b2/g2 (b7/g7 for black). This can obviously only
@@ -394,9 +395,10 @@ namespace {
   template<Color Us, bool Trace>
   Score evaluate_king(const Position& pos, const EvalInfo& ei) {
 
-    const Color Them = (Us == WHITE ? BLACK : WHITE);
+    const Color Them = (Us == WHITE ? BLACK   : WHITE);
+    const Square  Up = (Us == WHITE ? DELTA_N : DELTA_S);
 
-    Bitboard undefended, b, b1, b2, safe;
+    Bitboard undefended, b, b1, b2, safe, other;
     int attackUnits;
     const Square ksq = pos.square<KING>(Us);
 
@@ -442,43 +444,44 @@ namespace {
             attackUnits += QueenContactCheck * popcount(b);
         }
 
-        // Analyse the enemy's safe distance checks for sliders and knights
-        safe = ~(ei.attackedBy[Us][ALL_PIECES] | pos.pieces(Them));
 
-        b1 = pos.attacks_from<ROOK  >(ksq) & safe;
-        b2 = pos.attacks_from<BISHOP>(ksq) & safe;
+        // Analyse the safe enemy's checks which are possible on next move...
+        safe  = ~(ei.attackedBy[Us][ALL_PIECES] | pos.pieces(Them));
+
+        // ... and some other potential checks, only requiring the square to be
+        // safe from pawn-attacks, and not being occupied by a blocked pawn.
+        other = ~(   ei.attackedBy[Us][PAWN]
+                  | (pos.pieces(Them, PAWN) & shift_bb<Up>(pos.pieces(PAWN))));
+
+        b1 = pos.attacks_from<ROOK  >(ksq);
+        b2 = pos.attacks_from<BISHOP>(ksq);
 
         // Enemy queen safe checks
-        b = (b1 | b2) & ei.attackedBy[Them][QUEEN];
-        if (b)
-        {
-            attackUnits += QueenCheck;
-            score -= Checked;
-        }
+        if ((b1 | b2) & ei.attackedBy[Them][QUEEN] & safe)
+            attackUnits += QueenCheck, score -= SafeCheck;
 
-        // Enemy rooks safe checks
-        b = b1 & ei.attackedBy[Them][ROOK];
-        if (b)
-        {
-            attackUnits += RookCheck;
-            score -= Checked;
-        }
+        // Enemy rooks safe and other checks
+        if (b1 & ei.attackedBy[Them][ROOK] & safe)
+            attackUnits += RookCheck, score -= SafeCheck;
 
-        // Enemy bishops safe checks
-        b = b2 & ei.attackedBy[Them][BISHOP];
-        if (b)
-        {
-            attackUnits += BishopCheck;
-            score -= Checked;
-        }
+        else if (b1 & ei.attackedBy[Them][ROOK] & other)
+            score -= OtherCheck;
 
-        // Enemy knights safe checks
-        b = pos.attacks_from<KNIGHT>(ksq) & ei.attackedBy[Them][KNIGHT] & safe;
-        if (b)
-        {
-            attackUnits += KnightCheck;
-            score -= Checked;
-        }
+        // Enemy bishops safe and other checks
+        if (b2 & ei.attackedBy[Them][BISHOP] & safe)
+            attackUnits += BishopCheck, score -= SafeCheck;
+
+        else if (b2 & ei.attackedBy[Them][BISHOP] & other)
+            score -= OtherCheck;
+
+        // Enemy knights safe and other checks
+        b = pos.attacks_from<KNIGHT>(ksq) & ei.attackedBy[Them][KNIGHT];
+        if (b & safe)
+            attackUnits += KnightCheck, score -= SafeCheck;
+
+        else if (b & other)
+            score -= OtherCheck;
+		
         // To index KingDanger[] attackUnits must be in [0, 99] range
         attackUnits = std::min(399, std::max(0, attackUnits));
 
