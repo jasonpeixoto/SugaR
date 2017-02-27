18fcb1b25987eee1efb2c22d9061363d1594a593
Marco Zerbinati <zerbinati.marco@gmail.com>
2017-02-27 15:10:55 +0100

    Merge "Change definition of "weak" in threats calculation"
    Change definition of "weak" in threats calculation
    By defining "strongly protected" as "protected by a pawn, or protected
    by two pieces and not attacked by two enemy pieces".
    
    Passed STC:
    LLR: 2.97 (-2.94,2.94) [0.00,5.00]
    Total: 17050 W: 3128 L: 2931 D: 10991
    
    Passed LTC:
    LLR: 2.96 (-2.94,2.94) [0.00,5.00]
    Total: 120995 W: 15852 L: 15343 D: 89800
    
    Bench : 6269229
    
    Closes #1016
    master
    1 parent f1e3dfe commit 8f7e032b8c8c2c19e37ece09eecb589fe2342daa
    @snicolet snicolet committed

--- srcS/evaluate.cpp
+++ srcS/evaluate.cpp
@@ -179,10 +179,10 @@ namespace {
     S(-20,-12), S( 1, -8), S( 2, 10), S(  9, 10)
   };
   
-  // Protector[PieceType][distance] contains a protecting bonus for our king, 
+  // // Protector[PieceType][distance] contains a protecting bonus for our king,
   // indexed by piece type and distance between the piece and the king.
   const Score Protector[PIECE_TYPE_NB][8] = {
-  {}, {},
+    {}, {},
     { S(0, 0), S( 7, 9), S( 7, 1), S( 1, 5), S(-10,-4), S( -1,-4), S( -7,-3), S(-16,-10) }, // Knight
     { S(0, 0), S(11, 8), S(-7,-1), S(-1,-2), S( -1,-7), S(-11,-3), S( -9,-1), S(-16, -1) }, // Bishop
     { S(0, 0), S(10, 0), S(-2, 2), S(-5, 4), S( -6, 2), S(-14,-3), S( -2,-9), S(-12, -7) }, // Rook
@@ -690,7 +690,7 @@ namespace {
         } // rr != 0
 
 	    // Scale down bonus for candidate passers which need more than one pawn
-		// push to become passed.
+		// pawn push to become passed.
 		if (!pos.pawn_passed(Us, s + pawn_push(Us)))
 		    mbonus /= 2, ebonus /= 2;
 
@@ -701,7 +701,6 @@ namespace {
     if (DoTrace)
         Trace::add(PASSED, Us, score);
 
-    // Add the scores to the middlegame and endgame eval
     return score;
   }
 
