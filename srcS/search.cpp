c81ba9c8c9b3ae5249b86bc080f5b1d838cab3f8
Marco Zerbinati <zerbinati.marco@gmail.com>
2017-05-04 11:58:14 +0200

    FIX

--- srcS/search.cpp
+++ srcS/search.cpp
@@ -1047,7 +1047,7 @@ moves_loop: // When in check search starts from here
                   r += ONE_PLY;
 
               // Decrease/increase reduction for moves with a good/bad history
-              r = std::max(DEPTH_ZERO, (r / ONE_PLY - ss->history / 20000) * ONE_PLY);
+              r = std::max(0, (r / ONE_PLY - ss->history / (1000 + 3000 * depth / ONE_PLY))) * ONE_PLY;
           }
 		  
           // Set maximum reduction
