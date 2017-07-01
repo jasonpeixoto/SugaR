a30070340164f6be246db836ff1e730f3d6ea7ce
Marco Zerbinati <zerbinati.marco@gmail.com>
2017-07-01 21:09:34 +0200

    fix

---
+++ srcS/main.cpp
@@ -0,0 +1,54 @@
+/*
+  SugaR, a UCI chess playing engine derived from Stockfish
+  Copyright (C) 2004-2008 Tord Romstad (Glaurung author)
+  Copyright (C) 2008-2015 Marco Costalba, Joona Kiiski, Tord Romstad
+  Copyright (C) 2015-2017 Marco Costalba, Joona Kiiski, Gary Linscott, Tord Romstad
+
+  SugaR is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  SugaR is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <iostream>
+
+#include "bitboard.h"
+#include "evaluate.h"
+#include "position.h"
+#include "search.h"
+#include "thread.h"
+#include "uci.h"
+#include "syzygy/tbprobe.h"
+#include "tzbook.h"
+
+namespace PSQT {
+  void init();
+}
+
+int main(int argc, char* argv[]) {
+
+  std::cout << engine_info() << std::endl;
+
+  UCI::init(Options);
+  PSQT::init();
+  Bitboards::init();
+  Position::init();
+  Bitbases::init();
+  Eval::init();
+  Search::init();
+  Pawns::init();
+  Threads.init();
+  tzbook.init(Options["BookPath"]);
+  UCI::loop(argc, argv);
+
+  Threads.exit();
+  return 0;
+}
