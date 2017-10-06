a863ad2438e810a9b9fc70b8dc1034d6eb02df98
Козлов Сергей Александрович <kozlov.sergey.aleksandrovitch@gmail.com>
2017-10-06 20:35:20 +0300

    Windows version information added.

--- source/main.cpp
+++ source/main.cpp
@@ -39,54 +39,179 @@
 
 #include <time.h>
 
+#ifdef _WIN32
+#include <windows.h>
+#undef max
+#undef min
+
+#include "VersionHelpers.h"
+#endif
+
 namespace PSQT {
-  void init();
+	void init();
 }
 
 int main(int argc, char* argv[]) {
-	
-  {
-	const size_t time_length_const = 100;
-	char time_local[time_length_const];
-	memset(time_local, char(0), time_length_const);
-    time_t result = time(NULL);
-	tm tm_local;
-	errno_t errno_local = localtime_s(&tm_local, &result);
-	if (errno_local == 0)
+
 	{
-		errno_local = asctime_s(time_local, time_length_const, &tm_local);
+		const size_t time_length_const = 100;
+		char time_local[time_length_const];
+		memset(time_local, char(0), time_length_const);
+		time_t result = time(NULL);
+		tm tm_local;
+		errno_t errno_local = localtime_s(&tm_local, &result);
 		if (errno_local == 0)
 		{
-			std::cout << time_local;
+			errno_local = asctime_s(time_local, time_length_const, &tm_local);
+			if (errno_local == 0)
+			{
+				std::cout << time_local;
+			}
+			else
+			{
+				assert(errno_local != 0);
+			}
 		}
 		else
 		{
 			assert(errno_local != 0);
 		}
 	}
-	else
+
+	std::cout << engine_info() << std::endl;
+
+#ifdef _WIN32
 	{
-		assert(errno_local != 0);
+		SYSTEM_INFO siSysInfo;
+
+		// Copy the hardware information to the SYSTEM_INFO structure. 
+
+		GetSystemInfo(&siSysInfo);
+
+		// Display the contents of the SYSTEM_INFO structure. 
+
+		std::cout << "Hardware information: " << std::endl;
+		std::cout << "  OEM ID: " << siSysInfo.dwOemId << std::endl;
+		std::cout << "  Number of processors: " << siSysInfo.dwNumberOfProcessors << std::endl;
+		std::cout << "  Page size: " << siSysInfo.dwPageSize << std::endl;
+		std::cout << "  Processor type: " << siSysInfo.dwProcessorType << std::endl;
+		std::cout << "  Minimum application address: " << siSysInfo.lpMinimumApplicationAddress << std::endl;
+		std::cout << "  Maximum application address: " << siSysInfo.lpMaximumApplicationAddress << std::endl;
+		std::cout << "  Active processor mask: " << siSysInfo.dwActiveProcessorMask << std::endl;
+
+
+		{
+			InitVersion();
+
+			if (IsWindowsXPOrGreater())
+			{
+				if (IsWindowsXPSP1OrGreater())
+				{
+					if (IsWindowsXPSP2OrGreater())
+					{
+						if (IsWindowsXPSP3OrGreater())
+						{
+							if (IsWindowsVistaOrGreater())
+							{
+								if (IsWindowsVistaSP1OrGreater())
+								{
+									if (IsWindowsVistaSP2OrGreater())
+									{
+										if (IsWindows7OrGreater())
+										{
+											if (IsWindows7SP1OrGreater())
+											{
+												if (IsWindows8OrGreater())
+												{
+													if (IsWindows8Point1OrGreater())
+													{
+														if (IsWindows10OrGreater())
+														{
+															std::cout << "Windows 10 Or Greater" << std::endl;
+														}
+														else
+														{
+															std::cout << "Windows 8 Point 1 Or Greater" << std::endl;
+														}
+													}
+													else
+													{
+														std::cout << "Windows 8 Or Greater" << std::endl;
+													}
+												}
+												else
+												{
+													std::cout << "Windows 7 SP1 Or Greater" << std::endl;
+												}
+											}
+											else
+											{
+												std::cout << "Windows7 Or Greater" << std::endl;
+											}
+										}
+										else
+										{
+											std::cout << "Vista SP2 Or Greater" << std::endl;
+										}
+									}
+									else
+									{
+										std::cout << "Vista SP1O rGreater" << std::endl;
+									}
+								}
+								else
+								{
+									std::cout << "Vista Or Greater" << std::endl;
+								}
+							}
+							else
+							{
+								std::cout << "XP SP3 Or Greater" << std::endl;
+							}
+						}
+						else
+						{
+							std::cout << "XP SP2 Or Greater" << std::endl;
+						}
+					}
+					else
+					{
+						std::cout << "XP SP1 Or Greater" << std::endl;
+					}
+				}
+				else
+				{
+					std::cout << "XP Or Greater" << std::endl;
+				}
+			}
+
+			if (IsWindowsServer())
+			{
+				std::cout << "Server" << std::endl;
+			}
+			else
+			{
+				std::cout << "Client" << std::endl;
+			}
+		}
 	}
-  }
+#endif
 
-  std::cout << engine_info() << std::endl;
- 
-  UCI::init(Options);
-  PSQT::init();
-  Bitboards::init();
-  Position::init();
-  Bitbases::init();
-  Eval::init();
-  Search::init(Options["Clean Search"]);
-  Pawns::init();
-  Tablebases::init(Options["SyzygyPath"]);
-  TT.resize(Options["Hash"]);
-  Threads.init(Options["Threads"]);
-  Search::clear(); // After threads are up
-  tzbook.init(Options["BookPath"]);
-  UCI::loop(argc, argv);
+	UCI::init(Options);
+	PSQT::init();
+	Bitboards::init();
+	Position::init();
+	Bitbases::init();
+	Eval::init();
+	Search::init(Options["Clean Search"]);
+	Pawns::init();
+	Tablebases::init(Options["SyzygyPath"]);
+	TT.resize(Options["Hash"]);
+	Threads.init(Options["Threads"]);
+	Search::clear(); // After threads are up
+	tzbook.init(Options["BookPath"]);
+	UCI::loop(argc, argv);
 
-  Threads.exit();
-  return 0;
+	Threads.exit();
+	return 0;
 }
